e3614bff1afc11c04a1016318b081270d50074f8
jgart <jgart@dismail.de>
2021-04-23 03:58:38 -0400

    Add resume guix manifest.
    europasscv-bibliography is currently not packaged for guix.
    Let's package it!

---
+++ resume-manifest.scm
@@ -0,0 +1,10 @@
+;;; Missing package in guix:
+;; europasscv-bibliography
+(specifications->manifest
+  '("texlive-base"
+    "texlive-biblatex"
+    "texlive-charter"
+    "texlive-csquotes"
+    "texlive-helvetic"
+    "texlive-latex-babel"
+    "texlive-latex-hyperref"))
